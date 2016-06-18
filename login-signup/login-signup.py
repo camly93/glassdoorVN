1c2908a02d31413e6d0a6876daab310a8a125023
Nguyen Hoa Khanh <nghoakhanh89@gmail.com>
2016-06-18 22:16:26 +0700

    form review (chưa có flask)

--- login-signup/login-signup.py
+++ login-signup/login-signup.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def login():
     return render_template('login.html')
 
+@app.route('/review')
+def review():
+    return render_template('input-review.html')
+
 
 if __name__ == '__main__':
     app.run()
