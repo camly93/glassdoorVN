7cae217d8dfda2a23632b31ff24bbf053a7d81c0
Ly Nguyen <camly93@gmail.com>
2016-06-19 13:34:16 +0700

    update 1:34 pm

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 from flask import Flask,render_template,request, redirect
 import pymongo
 db_url = "mongodb://Raventine:raven123@ds013564.mlab.com:13564/job-offer"
 db = pymongo.MongoClient(db_url).get_default_database()
-=======
+
 from pymongo import MongoClient
 client = MongoClient('mongodb://Raventine:raven123@ds013564.mlab.com:13564/job-offer')
 db=client['job-offer']
@@ -30,7 +29,6 @@ for x in range (len(litt)//a+1):
 
 from flask import Flask,render_template,request, redirect,url_for,make_response
 
->>>>>>> fed92c35a4e44c98dff5ec8f0f50f0bc2c261cf7
 app = Flask(__name__, static_url_path ='/static')
 
 #trang LOG IN
@@ -41,7 +39,7 @@ def login():
     if request.method == 'POST':
         for x in account:
             if request.form['username']==x['username'] and x['password']==request.form['password']:
-                return redirect('/Home.html')
+                return redirect('/home')
     return render_template('Login.html')
 
 #trang ABOUT US
@@ -54,7 +52,7 @@ def aboutus():
 def home():
     if request.method == 'POST':
         print("someone is searching...")
-        return redirect('/company')
+        return redirect('/company-list')
     return render_template('Home.html')
 
 #trang COMPANY REVIEW
@@ -62,21 +60,12 @@ def home():
 def signup():
     return render_template('Company-review.html')
 
-<<<<<<< HEAD
-# @app.route('/review-form')
-# def review_form():
-#     return render_template('Form-review.html')
-
 @app.route('/review-form', methods=['POST','GET'])
 def review_form2():
     collection=db['company-review']
     if request.method == 'POST':
         collection.insert_one({'Cong ty': request.form['data_1'],'Xep hang': request.form['data_2'],'Tieu de': request.form['data_3'],'Diem cong': request.form['data_4'],'Diem tru': request.form['data_5'],'Trai nghiem': request.form['data_6'],'Gop y': request.form['data_7']})
-    return render_template('Form-review2.html')
-=======
-#trang REVIEW FORM
-@app.route('/review-form')
-def review_form2():
+        return redirect('/company-review')
     return render_template('Form-review.html')
 
 #trang COMPANY LIST
@@ -88,7 +77,6 @@ def search(a=pagelist[0],current=1,c=len(pagelist)):
         return render_template('Company-list.html', everything=a, manypage=c, current=current)
     return render_template('Company-list.html',everything=a,manypage=c,current=current)
     # return render_template('Login.html')
->>>>>>> fed92c35a4e44c98dff5ec8f0f50f0bc2c261cf7
 
 if __name__ == '__main__':
     app.run()
