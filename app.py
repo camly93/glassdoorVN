50e8db4ddd59144e9eb6f6390019f36422a9c14f
Nguyen Hoa Khanh <nghoakhanh89@gmail.com>
2016-06-19 12:54:11 +0700

    up review len mlab

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template,request, redirect
-
+import pymongo
+db_url = "mongodb://Raventine:raven123@ds013564.mlab.com:13564/job-offer"
+db = pymongo.MongoClient(db_url).get_default_database()
 app = Flask(__name__, static_url_path ='/static')
 
 @app.route('/', methods=['GET','POST'])
@@ -28,8 +30,11 @@ def signup():
 # def review_form():
 #     return render_template('Form-review.html')
 
-@app.route('/review-form')
+@app.route('/review-form', methods=['POST','GET'])
 def review_form2():
+    collection=db['company-review']
+    if request.method == 'POST':
+        collection.insert_one({'Cong ty': request.form['data_1'],'Xep hang': request.form['data_2'],'Tieu de': request.form['data_3'],'Diem cong': request.form['data_4'],'Diem tru': request.form['data_5'],'Trai nghiem': request.form['data_6'],'Gop y': request.form['data_7']})
     return render_template('Form-review2.html')
 
 if __name__ == '__main__':
